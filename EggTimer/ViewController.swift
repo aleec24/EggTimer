992a177c9a6913cad4893214d883007ceb92aa69
aleec24 <78573991+aleec24@users.noreply.github.com>
2023-02-09 12:04:39 -0600

    Add files via upload
    Initial Project

---
+++ EggTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Alexa Cabalceta on 27/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
