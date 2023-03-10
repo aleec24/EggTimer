05a3c857ac40922a4432f751277a0cdae3a7e53f
Alexa <alexa_cabalceta08@hotmail.com>
2023-03-10 14:44:16 -0600

    Simplifying code with dictionary

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -8,6 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+    
+    @IBAction func hardnessSelectors(_ sender: UIButton) {
+        
+        let hardness = sender.currentTitle!
+        
+        print("Cooking time is \(eggTimes[hardness]!) minutes")
+        }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
