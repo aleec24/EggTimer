cf77c1b68508a36456ec79eb0656550c2ee59c55
Alexa <alexa_cabalceta08@hotmail.com>
2023-03-13 11:58:20 -0600

    Adding timer function.

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,21 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var titleLabel: UILabel!
+    var secondsRemaining = 60
+    var eggCountdown = 0
+    var timer = Timer()
     let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
     
     @IBAction func hardnessSelectors(_ sender: UIButton) {
         
-        let hardness = sender.currentTitle!
+        timer.invalidate()
         
-        print("Cooking time is \(eggTimes[hardness]!) minutes")
+        let hardness = sender.currentTitle!
+        let result = eggTimes[hardness]!
+        eggCountdown = result * secondsRemaining
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
+            if self.eggCountdown > 0 {
+                print ("\(self.eggCountdown) seconds.")
+                self.eggCountdown -= 1
+            } else {
+                Timer.invalidate()
+                self.titleLabel.text = "DONE!"
+            }
         }
+        
+    }
     
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
 }
 
